cb39aac52d2454e43d0a3d0ebd10866db38aecbd
xuchen <1009559594@qq.com>
2020-04-10 18:20:07 +0800

    增加t_talent字段open_id

--- common-lib/src/main/java/com/talentcard/common/mapper/TalentMapper.java
+++ common-lib/src/main/java/com/talentcard/common/mapper/TalentMapper.java
@@ -3,10 +3,14 @@ package com.talentcard.common.mapper;
 import com.talentcard.common.pojo.TalentPO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.HashMap;
+
 /**
  * TalentMapper继承基类
  */
 @Mapper
 public interface TalentMapper extends BaseMapper<TalentPO, Long> {
     Integer add(TalentPO talentPO);
+    HashMap<String, Object> findRegisterOne(Long openId);
+
 }
