390a20e3fc6e09647f75f44f3f388b2f4088715e
xuchen <1009559594@qq.com>
2020-04-10 14:32:44 +0800

    增加字段 current type

--- talentfront/src/main/java/com/talentcard/front/service/impl/TalentServiceImpl.java
+++ talentfront/src/main/java/com/talentcard/front/service/impl/TalentServiceImpl.java
@@ -2,14 +2,13 @@ package com.talentcard.front.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
 import com.talentcard.common.mapper.*;
-import com.talentcard.common.pojo.CertificationPO;
-import com.talentcard.common.pojo.EducationPO;
-import com.talentcard.common.pojo.TalentPO;
+import com.talentcard.common.pojo.*;
 import com.talentcard.common.vo.ResultVO;
 import com.talentcard.front.service.ITalentService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
+
 
 public class TalentServiceImpl implements ITalentService {
     @Autowired
@@ -22,8 +21,11 @@ public class TalentServiceImpl implements ITalentService {
     private ProfTitleMapper profTitleMapper;
     @Autowired
     private ProfQualityMapper profQualityMapper;
+
     @Override
     public ResultVO register(JSONObject jsonObject) {
+        //设置状态值 状态3为注册中
+        Byte status = (byte)3;
         //人才表
         TalentPO talentPO = new TalentPO();
         talentPO.setName(jsonObject.getString("name"));
@@ -34,21 +36,44 @@ public class TalentServiceImpl implements ITalentService {
         talentPO.setPhone(jsonObject.getString("phone"));
         talentPO.setCreateTime(new Date());
         talentMapper.add(talentPO);
-//        Long talentId = talentPO.getTalentId();
+        Long talentId = talentPO.getTalentId();
 
-//        //认证表
-//        CertificationPO certificationPO = new CertificationPO();
-//        certificationPO.setCreateTime(new Date());
-//        certificationPO.setStatus((byte)1);
-//        certificationPO.setTalentId(talentId);
-//        certificationMapper.add(certificationPO);
-//        Long certificationId = certificationPO.getCertId();
-//
-//        //学历表
-//        EducationPO educationPO = new EducationPO();
-//        educationPO.setEducation(jsonObject.getInteger("education"));
-//        educationPO.setSchool(jsonObject.getString("school"));
-//        educationPO.setFristClass(jsonObject.getByte("firstClass"));
+        //认证表
+        CertificationPO certificationPO = new CertificationPO();
+        certificationPO.setCreateTime(new Date());
+        certificationPO.setStatus(status);
+        certificationPO.setTalentId(talentId);
+        certificationMapper.add(certificationPO);
+        Long certificationId = certificationPO.getCertId();
+
+        //学历表
+        EducationPO educationPO = new EducationPO();
+        educationPO.setEducation(jsonObject.getInteger("education"));
+        educationPO.setSchool(jsonObject.getString("school"));
+        educationPO.setFristClass(jsonObject.getByte("firstClass"));
+        educationPO.setMajor(jsonObject.getString("major"));
+        educationPO.setCertId(certificationId);
+        educationPO.setTalentId(talentId);
+        educationPO.setStatus(status);
+        educationMapper.insertSelective(educationPO);
+
+        //职称表
+        ProfTitlePO profTitlePO = new ProfTitlePO();
+        profTitlePO.setCategory(jsonObject.getInteger(" profTitleCategory"));
+        profTitlePO.setInfo(jsonObject.getString("profTitleInfo"));
+        profTitlePO.setCertId(certificationId);
+        profTitlePO.setTalentId(talentId);
+        profTitlePO.setStatus(status);
+        profTitleMapper.insertSelective(profTitlePO);
+
+        //职业资格表
+        ProfQualityPO profQualityPO = new ProfQualityPO();
+        profQualityPO.setCategory(jsonObject.getInteger("profQualityCategory"));
+        profQualityPO.setInfo(jsonObject.getString("profQualityInfo"));
+        profQualityPO.setCertId(certificationId);
+        profQualityPO.setTalentId(talentId);
+        profQualityPO.setStatus(status);
+        profQualityMapper.insertSelective(profQualityPO);
         return new ResultVO(1000);
     }
 }
