88c4711b8f7bd1bc8c3f168a0c52af0d3a55702e
xiahui <hui.xia@rpahelper.com>
2020-04-10 14:17:37 +0800

    Rebuild talentcard.

--- common-lib/src/main/java/com/talentcard/common/mapper/ProfQualityMapper.java
+++ common-lib/src/main/java/com/talentcard/common/mapper/ProfQualityMapper.java
@@ -3,9 +3,18 @@ package com.talentcard.common.mapper;
 import com.talentcard.common.pojo.ProfQualityPO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * ProfQualityMapper继承基类
  */
 @Mapper
 public interface ProfQualityMapper extends BaseMapper<ProfQualityPO, Long> {
+    /**
+     * 根据人才ID查询
+     *
+     * @param talentId
+     * @return
+     */
+    List<Integer> queryNameByTalentId(Long talentId);
 }
--- common-lib/src/main/java/com/talentcard/common/mapper/ProfTitleMapper.java
+++ common-lib/src/main/java/com/talentcard/common/mapper/ProfTitleMapper.java
@@ -3,9 +3,18 @@ package com.talentcard.common.mapper;
 import com.talentcard.common.pojo.ProfTitlePO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * ProfTitleMapper继承基类
  */
 @Mapper
 public interface ProfTitleMapper extends BaseMapper<ProfTitlePO, Long> {
+    /**
+     * 根据人才ID查询
+     *
+     * @param talentId
+     * @return
+     */
+    List<Integer> queryNameByTalentId(Long talentId);
 }
