1302c840e8e2449ab9fb81fd3e538f59670d2945
xiahui <hui.xia@rpahelper.com>
2020-04-14 18:32:19 +0800

    Rebuild talentcard, modify policy.

--- talentweb/src/main/java/com/talentcard/web/vo/PolicyVO.java
+++ talentweb/src/main/java/com/talentcard/web/vo/PolicyVO.java
@@ -1,9 +1,12 @@
 package com.talentcard.web.vo;
 
+import com.talentcard.common.pojo.PolicyPO;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author: xiahui
@@ -39,4 +42,35 @@ public class PolicyVO implements Serializable {
      * 备注信息
      */
     private String desc;
+
+    /**
+     * pos 转 vos
+     *
+     * @param pos
+     * @return
+     */
+    public static List<PolicyVO> convert(List<PolicyPO> pos) {
+        List<PolicyVO> vos = new ArrayList<>();
+        for (PolicyPO po : pos) {
+            vos.add(PolicyVO.convert(po));
+        }
+        return vos;
+    }
+
+    public static PolicyVO convert(PolicyPO po) {
+        PolicyVO vo = new PolicyVO();
+
+        vo.setCreateTime(po.getCreateTime());
+        vo.setName(po.getName());
+        vo.setNum(po.getNum());
+        vo.setApply(po.getApply() == 1 ? "需要" : "不需要");
+        String frequency = "无";
+        if (null != po.getRate() && null != po.getUnit() && null != po.getTimes()) {
+            frequency = po.getRate() + (po.getUnit() == 1 ? "年/" : po.getUnit() == 2 ? "季/" : "月/") + po.getTimes() + "次";
+        }
+        vo.setFrequency(frequency);
+        vo.setDesc(po.getDescription());
+
+        return vo;
+    }
 }
