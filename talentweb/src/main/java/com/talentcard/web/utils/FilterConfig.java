11ce651cfcbf75f3821d53ead58b57b454a25dcb
chaojie.jiang <chaojie.jiang@vbooster.cn>
2020-04-20 15:14:02 +0800

    冲突

---
+++ talentweb/src/main/java/com/talentcard/web/utils/FilterConfig.java
@@ -0,0 +1,10 @@
+package com.talentcard.web.utils;
+
+/**
+ * @author: jiangzhaojie
+ * @date: Created in 8:46 2020/4/20
+ * @version: 1.0.0
+ * @description:
+ */
+public class FilterConfig {
+}
---
+++ talentweb/src/main/java/com/talentcard/web/utils/SpringMVCConfig.java
@@ -0,0 +1,10 @@
+package com.talentcard.web.utils;
+
+/**
+ * @author: jiangzhaojie
+ * @date: Created in 8:57 2020/4/20
+ * @version: 1.0.0
+ * @description:
+ */
+public class SpringMVCConfig {
+}
