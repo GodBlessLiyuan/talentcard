92c047d5839385a393bce9d13536915ce1373ae5
enyong.jiang <enyong.jiang@vbooster.cn>
2020-04-20 11:33:34 +0800

    接收微信服务器消息

---
+++ wechat/src/main/java/com/talentcard/wechat/service/WxCardService.java
@@ -0,0 +1,16 @@
+package com.talentcard.wechat.service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * @author: jiangenyong
+ * @date: Created in 2020-04-20 9:38
+ * @description: TODO
+ * @version: 1.0
+ */
+public interface WxCardService {
+    public Map<String,String> parseRequest(HttpServletRequest request);
+
+    public boolean check(String timestamp,String nonce,String signature);
+}
