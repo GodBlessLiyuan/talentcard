4f790c8bf97c5522cf565943c22ce0edb2ceca1b
xiahui <hui.xia@rpahelper.com>
2020-04-28 11:55:02 +0800

    Rebuild talentcard.

--- talenteureka/src/main/java/com/talentcard/eureka/EurekaApplication.java
+++ talenteureka/src/main/java/com/talentcard/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.talentcard.talenteureka;
+package com.talentcard.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
