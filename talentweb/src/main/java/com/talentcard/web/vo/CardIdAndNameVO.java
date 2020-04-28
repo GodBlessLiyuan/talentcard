7df7b2f0e7d4d0ce4923c65b62d293a294719430
chaojie.jiang <chaojie.jiang@vbooster.cn>
2020-04-28 15:31:26 +0800

    cardIdNameVO

---
+++ talentweb/src/main/java/com/talentcard/web/vo/CardIdAndNameVO.java
@@ -0,0 +1,30 @@
+package com.talentcard.web.vo;
+
+import com.talentcard.common.pojo.CardPO;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: jiangzhaojie
+ * @date: Created in 15:03 2020/4/28
+ * @version: 1.0.0
+ * @description: 提取出card表中的cardId和其相对应的名字
+ */
+@Data
+public class CardIdAndNameVO {
+    private Long cardId;
+    private String cardName;
+
+    public static List<CardIdAndNameVO> convert(List<CardPO> pos) {
+        List<CardIdAndNameVO> vos = new ArrayList<>();
+        for (CardPO po : pos) {
+            CardIdAndNameVO vo = new CardIdAndNameVO();
+            vo.setCardId(po.getCardId());
+            vo.setCardName(po.getName());
+            vos.add(vo);
+        }
+        return vos;
+    }
+}
