995830f5dd816ea2bdadeff5b07d37d0c9e77768
xiahui <hui.xia@rpahelper.com>
2020-04-28 11:32:05 +0800

    Talentcard add spring boot admin.

--- talentadmin/src/main/java/com/talentcard/admin/AdminApplication.java
+++ talentadmin/src/main/java/com/talentcard/admin/AdminApplication.java
@@ -1,10 +1,21 @@
 package com.talentcard.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @author: xiahui
  * @date: Created in 2020/4/28 10:02
  * @description: Spring Boot Admin 监控
  * @version: 1.0
  */
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
 public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
 }
---
+++ talenteureka/src/main/java/com/talentcard/talenteureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.talentcard.talenteureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2020/4/28 11:22
+ * @description: Eureka 注册发现
+ * @version: 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
