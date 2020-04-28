d7057ac774d8b51a6baee0d4ac94cd38eee176c2
xiahui <hui.xia@rpahelper.com>
2020-04-28 10:09:22 +0800

    Rebuild talentcard, add spring boot admin struct.

---
+++ talentadmin/src/main/java/com/talentcard/admin/AdminApplication.java
@@ -0,0 +1,10 @@
+package com.talentcard.admin;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2020/4/28 10:02
+ * @description: Spring Boot Admin 监控
+ * @version: 1.0
+ */
+public class AdminApplication {
+}
