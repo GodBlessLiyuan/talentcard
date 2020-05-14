d799c1e6d894a373ed8e46d0289879c55f762a09
xuchen <1009559594@qq.com>
2020-05-14 10:50:01 +0800

    领取高级卡之前判断是否有脏数据

--- common-lib/src/main/java/com/talentcard/common/mapper/CertificationMapper.java
+++ common-lib/src/main/java/com/talentcard/common/mapper/CertificationMapper.java
@@ -17,6 +17,7 @@ import java.util.Map;
 public interface CertificationMapper extends BaseMapper<CertificationPO, Long> {
     /**
      * insert返回主键
+     *
      * @param certificationPO
      * @return
      */
@@ -24,6 +25,7 @@ public interface CertificationMapper extends BaseMapper<CertificationPO, Long> {
 
     /**
      * 进行人才的审批状态查询
+     *
      * @param map
      * @return
      */
@@ -31,20 +33,23 @@ public interface CertificationMapper extends BaseMapper<CertificationPO, Long> {
 
     /**
      * 根据用户姓名查询当前用的认证审批信息
+     *
      * @param tanlentId
      * @return
      */
-    ApprovalBO queryAllMsg(@Param("talentId")Long tanlentId,@Param("certId")Long certId);
+    ApprovalBO queryAllMsg(@Param("talentId") Long tanlentId, @Param("certId") Long certId);
 
     /**
      * 查看详情
+     *
      * @param tanlentId
      * @return
      */
-    ApprovalBO queryAllMsgLook(@Param("talentId")Long tanlentId,@Param("certId")Long certId);
+    ApprovalBO queryAllMsgLook(@Param("talentId") Long tanlentId, @Param("certId") Long certId);
 
     /**
      * 根据talentId，更新认证表从一个状态更新到另一个状态
+     *
      * @param talentId
      * @param currentStatus
      * @param status
@@ -55,6 +60,7 @@ public interface CertificationMapper extends BaseMapper<CertificationPO, Long> {
 
     /**
      * 根据talentId，找到c表里status=1，正常使用的信息
+     *
      * @param talentId
      * @return
      */
@@ -64,6 +70,7 @@ public interface CertificationMapper extends BaseMapper<CertificationPO, Long> {
      * 判断旧卡是基本卡还是高级卡，基本卡就改为失效9，高级卡改为失效10
      * 查询状态=9的数量，如果是0，说明这个是注册的基本卡换高级卡
      * 如果不为0，说明这个是高级卡换高级卡
+     *
      * @param openId
      * @return
      */
@@ -71,6 +78,7 @@ public interface CertificationMapper extends BaseMapper<CertificationPO, Long> {
 
     /**
      * 查询status=3的数据，查看是否已经拥有待审批数据了
+     *
      * @param openId
      * @return
      */
@@ -81,15 +89,25 @@ public interface CertificationMapper extends BaseMapper<CertificationPO, Long> {
      * 后台左侧栏位的小红点
      * c表status=3：待审批
      * type=2：初级卡换高级卡
+     *
      * @return
      */
     Integer findWaitApprovalNum();
 
     /**
      * 根据认证ID更新C表中的认证状态
+     *
      * @param certId
      * @param status
      * @return
      */
     int updateStatusByCertId(@Param("certId") Long certId, @Param("status") Byte status);
+
+    /**
+     * 发高级卡之前判断数之前据是否为脏数据
+     *
+     * @return
+     */
+    Integer checkIfDirty(@Param("talentId") Long talentId,
+                         @Param("cStatus") Byte cStatus, @Param("ucStatus") Byte ucStatus);
 }
