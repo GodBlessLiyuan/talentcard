f08e69f14adf58a54756b73477917270bb0b1a83
xuchen <1009559594@qq.com>
2020-05-14 08:58:17 +0800

    Toekn时间写在配置文件里

--- wechat/src/main/java/com/talentcard/wechat/utils/AccessTokenUtil.java
+++ wechat/src/main/java/com/talentcard/wechat/utils/AccessTokenUtil.java
@@ -30,6 +30,7 @@ public class AccessTokenUtil {
     @Autowired
     private RedisTemplate redisTemplate;
     private static RedisTemplate myRedis;
+    private static Long accessTokenTime;
 
     /**
      * 构造方法执行后的初始化
@@ -54,7 +55,7 @@ public class AccessTokenUtil {
         //判断拿到accessToken是否为空，若为空，抛异常
         //不为空，则如下，更新accessToken，且记录当前时间
         if (applyAccessToken != null) {
-            myRedis.opsForValue().set("accessToken", applyAccessToken, 2L, TimeUnit.MINUTES);
+            myRedis.opsForValue().set("accessToken", applyAccessToken, accessTokenTime, TimeUnit.MINUTES);
             logger.info("成功拿到accessToken：{}", applyAccessToken);
         } else {
             throw new WechatException("token拿不到");
@@ -84,6 +85,11 @@ public class AccessTokenUtil {
     @Value("${wechat.appSecret}")
     private void setAppSecret(String appSecret) {
         AccessTokenUtil.appSecret = appSecret;
+    }
+
+    @Value("${wechat.accessTokenTime}")
+    private void setAccessTokenTime(Long accessTokenTime) {
+        AccessTokenUtil.accessTokenTime = accessTokenTime;
     }
 }
 
