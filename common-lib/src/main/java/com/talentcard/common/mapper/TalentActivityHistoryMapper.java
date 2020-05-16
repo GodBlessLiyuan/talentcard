fd5e70f97f7dc5eae3b8b73ca4548aefb5fd6ca2
xuchen <1009559594@qq.com>
2020-05-16 15:36:49 +0800

    农家乐排除掉总次数

--- common-lib/src/main/java/com/talentcard/common/mapper/TalentActivityHistoryMapper.java
+++ common-lib/src/main/java/com/talentcard/common/mapper/TalentActivityHistoryMapper.java
@@ -16,13 +16,14 @@ public interface TalentActivityHistoryMapper extends BaseMapper<TalentActivityHi
     /**
      * 根据员工openID,一级目录id和二级目录id
      * 得到检验通过次数
+     *
      * @param activityFirstContentId
      * @param activitySecondContentId
      * @param startTime
      * @param endTime
      * @return
      */
-    Long getVertifyNum( @Param("staffId") Long staffId,
+    Long getVertifyNum(@Param("staffId") Long staffId,
                        @Param("activityFirstContentId") Long activityFirstContentId,
                        @Param("activitySecondContentId") Long activitySecondContentId,
                        @Param("startTime") String startTime,
@@ -30,10 +31,11 @@ public interface TalentActivityHistoryMapper extends BaseMapper<TalentActivityHi
 
     /**
      * 根据开始结束时间，决定一定时间段内，消耗的总次数，如景区这种消耗类型的活动等
+     *
      * @param startTime
      * @param endTime
      * @return
      */
     Long getCostTimes(@Param("startTime") String startTime,
-                        @Param("endTime") String endTime);
+                      @Param("endTime") String endTime);
 }
