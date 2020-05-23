a348d098122d7e8f1fc8cfba4aad63bb5149d521
haoliang.cui <haoliang.cui@godinsec.com>
2020-05-23 17:17:07 +0800

    添加junit测试demo

---
+++ talentweb/src/test/java/com/talentcard/web/WebApplicationTest.java
@@ -0,0 +1,41 @@
+package com.talentcard.web;
+
+import com.talentcard.common.mapper.AuthorityMapper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@ActiveProfiles("dev")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = WebApplication.class)
+public class WebApplicationTest {
+
+    //authority
+    @Autowired
+    private AuthorityMapper authorityMapper;
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testQuery() {
+        String name = authorityMapper.queryByAuthorityId(1);
+        System.out.println("1123");
+        System.out.println(name);
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
