a7118f48625c9e6c4ff46ff0fbc8145c96ce701a
haoliang.cui <haoliang.cui@godinsec.com>
2020-05-25 23:26:41 +0800

    修复启动崩溃

--- wechat/src/main/java/com/talentcard/wechat/WechatApplication.java
+++ wechat/src/main/java/com/talentcard/wechat/WechatApplication.java
@@ -2,13 +2,18 @@ package com.talentcard.wechat;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
+@AutoConfigurationPackage
 @MapperScan("com.talentcard.common.mapper")
-@MapperScan("com.talentcard.common.utils.redis")
+@ComponentScan("com.talentcard.common.utils.redis")
 @SpringBootApplication
+@EnableConfigurationProperties()
 public class WechatApplication {
     public static void main(String[] args) {
         SpringApplication.run(WechatApplication.class, args);
