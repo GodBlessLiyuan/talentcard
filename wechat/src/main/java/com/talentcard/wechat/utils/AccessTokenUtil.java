7bbbdd6f3217bdc569e49efc4fc07b9797583689
xuchen <1009559594@qq.com>
2020-05-29 10:25:42 +0800

    accessToken key改名

--- wechat/src/main/java/com/talentcard/wechat/utils/AccessTokenUtil.java
+++ wechat/src/main/java/com/talentcard/wechat/utils/AccessTokenUtil.java
@@ -55,7 +55,7 @@ public class AccessTokenUtil {
         //判断拿到accessToken是否为空，若为空，抛异常
         //不为空，则如下，更新accessToken，且记录当前时间
         if (applyAccessToken != null) {
-            myRedis.opsForValue().set("accessToken", applyAccessToken, accessTokenTime, TimeUnit.MINUTES);
+            myRedis.opsForValue().set("wechatAccessToken", applyAccessToken, accessTokenTime, TimeUnit.MINUTES);
             logger.info("成功拿到accessToken：{}", applyAccessToken);
         } else {
             throw new WechatException("token拿不到");
@@ -64,10 +64,10 @@ public class AccessTokenUtil {
 
     public static String getAccessToken() throws WechatException {
         //一小时，更换accessToken
-        String accessToken = (String) myRedis.opsForValue().get("accessToken");
+        String accessToken = (String) myRedis.opsForValue().get("wechatAccessToken");
         if (accessToken == null || accessToken.equals("")) {
             AccessTokenUtil.applyAccessToken();
-            accessToken = (String) myRedis.opsForValue().get("accessToken");
+            accessToken = (String) myRedis.opsForValue().get("wechatAccessToken");
         }
         return accessToken;
     }
