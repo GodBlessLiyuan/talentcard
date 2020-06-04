ab261f341904fe3684acea21ddd274cc6959ef61
xuchen <1009559594@qq.com>
2020-06-04 16:46:59 +0800

    jsToken

--- miniprogram/src/main/java/com/talentcard/miniprogram/pojo/JsTokenPO.java
+++ miniprogram/src/main/java/com/talentcard/miniprogram/pojo/JsTokenPO.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class JsTokenPO {
-    private String access_token;
-    private Long expires_in;
-    private String refresh_token;
     private String openid;
-    private String scope;
+    private Long session_key;
+    private String unionid;
+    private String errcode;
+    private String errmsg;
 }
