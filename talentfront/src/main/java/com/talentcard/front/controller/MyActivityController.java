ab323f39b6ca987c6ff8fbeceb4fa099794bb6da
xuchen <1009559594@qq.com>
2020-06-08 10:58:33 +0800

    全局搜索

--- talentfront/src/main/java/com/talentcard/front/controller/MyActivityController.java
+++ talentfront/src/main/java/com/talentcard/front/controller/MyActivityController.java
@@ -3,10 +3,14 @@ package com.talentcard.front.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.talentcard.common.vo.ResultVO;
 import com.talentcard.front.service.IMyActivityService;
+import com.talentcard.front.service.ITalentFarmhouseService;
+import com.talentcard.front.service.ITalentTripService;
 import com.talentcard.front.utils.VerificationCodeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
 
 /**
  * @author ChenXU
@@ -19,6 +23,10 @@ import org.springframework.web.multipart.MultipartFile;
 public class MyActivityController {
     @Autowired
     private IMyActivityService iMyActivityService;
+    @Autowired
+    private ITalentTripService iTalentTripService;
+    @Autowired
+    private ITalentFarmhouseService iTalentFarmhouseService;
 
     /**
      * 添加意见反馈
@@ -73,5 +81,35 @@ public class MyActivityController {
     @PostMapping("findMyCollect")
     public ResultVO findMyCollect(@RequestParam(value = "openId") String openId) {
         return iMyActivityService.findMyCollect(openId);
+    }
+
+    /**
+     * 全局搜索
+     *
+     * @param openId
+     * @param name
+     * @return
+     */
+    @PostMapping("queryOverall")
+    public ResultVO queryOverall(@RequestParam(value = "openId") String openId,
+                                 @RequestParam(value = "name", required = false, defaultValue = "") String name) {
+        ResultVO tripResultVO = iTalentTripService.findSecondContent(openId, name, null, null, (byte) 1);
+        ResultVO farmhouseResultVO = iTalentFarmhouseService.findSecondContent(openId, name, null, (byte) 1);
+        HashMap<String, Object> result = new HashMap<>();
+        HashMap<String, Object> tripResult;
+        HashMap<String, Object> farmhouseResult;
+        if (tripResultVO != null) {
+            tripResult = (HashMap<String, Object>) tripResultVO.getData();
+            if (tripResult.get("scenicList") != null) {
+                result.put("tripResult", tripResult.get("scenicList"));
+            }
+        }
+        if (farmhouseResultVO != null) {
+            farmhouseResult = (HashMap<String, Object>) farmhouseResultVO.getData();
+            if (farmhouseResult.get("farmhouseVOList") != null) {
+                result.put("farmhouseResult", farmhouseResult.get("farmhouseVOList"));
+            }
+        }
+        return new ResultVO(1000, result);
     }
 }
