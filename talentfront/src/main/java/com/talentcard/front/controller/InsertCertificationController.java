a8c1ab444c870ba88dbc2fd3e7f82c39297cadaf
xuchen <1009559594@qq.com>
2020-06-23 14:19:27 +0800

    临时

--- talentfront/src/main/java/com/talentcard/front/controller/InsertCertificationController.java
+++ talentfront/src/main/java/com/talentcard/front/controller/InsertCertificationController.java
@@ -21,26 +21,51 @@ public class InsertCertificationController {
     @Autowired
     IInsertCertificationService iInsertCertificationService;
 
+    /**
+     * 学历
+     * @param educationDTO
+     * @return
+     */
     @RequestMapping("addEducation")
     public ResultVO addEducation(@RequestBody EducationDTO educationDTO) {
         return iInsertCertificationService.addEducation(educationDTO);
     }
 
+    /**
+     * 职业资格
+     * @param profQualityDTO
+     * @return
+     */
     @RequestMapping("addProfQuality")
     public ResultVO addProfQuality(@RequestBody ProfQualityDTO profQualityDTO) {
         return iInsertCertificationService.addProfQuality(profQualityDTO);
     }
 
+    /**
+     * 职称
+     * @param profTitleDTO
+     * @return
+     */
     @RequestMapping("addProfTitle")
     public ResultVO addProfTitle(@RequestBody ProfTitleDTO profTitleDTO) {
         return iInsertCertificationService.addProfTitle(profTitleDTO);
     }
 
+    /**
+     * 人才荣誉
+     * @param talentHonourDTO
+     * @return
+     */
     @RequestMapping("addTalentHonour")
     public ResultVO addTalentHonour(@RequestBody TalentHonourDTO talentHonourDTO) {
         return iInsertCertificationService.addTalentHonour(talentHonourDTO);
     }
 
+    /**
+     * 删除
+     * @param insertCertId
+     * @return
+     */
     @RequestMapping("delete")
     public ResultVO delete(@RequestParam(value = "insertCertId") Long insertCertId) {
         return iInsertCertificationService.delete(insertCertId);
