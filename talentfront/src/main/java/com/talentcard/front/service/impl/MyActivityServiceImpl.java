330b97f891854610a356c6b2656216ff1ee22111
haoliang.cui <haoliang.cui@rpahelper.com>
2020-06-30 15:36:47 +0800

    删除无用代码

--- talentfront/src/main/java/com/talentcard/front/service/impl/MyActivityServiceImpl.java
+++ talentfront/src/main/java/com/talentcard/front/service/impl/MyActivityServiceImpl.java
@@ -15,7 +15,6 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.lang.invoke.ConstantCallSite;
 import java.util.Date;
 import java.util.List;
 
@@ -27,8 +26,6 @@ import java.util.List;
  */
 @Service
 public class MyActivityServiceImpl implements IMyActivityService {
-    @Autowired
-    private FilePathConfig filePathConfig;
     @Autowired
     private FeedbackMapper feedbackMapper;
     @Autowired
