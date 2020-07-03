b811c6ce9a49908c8370a75104c1a4521123bc82
haoliang.cui <haoliang.cui@rpahelper.com>
2020-07-03 19:01:40 +0800

    修复批量导入文案错误的bug

--- talentweb/src/main/java/com/talentcard/web/utils/BatchCertificateUtil.java
+++ talentweb/src/main/java/com/talentcard/web/utils/BatchCertificateUtil.java
@@ -23,19 +23,20 @@ public class BatchCertificateUtil {
         talentHonourMap.put("1", "企业高管");
         talentHonourMap.put("2", "文化名家");
         talentHonourMap.put("3", "非遗传承人");
-        talentHonourMap.put("4", "域外乡贤");
+        talentHonourMap.put("4", "寓外乡贤");
         talentHonourMap.put("5", "乡土人才");
+        talentHonourMap.put("6", "无");
 
         talentCategoryMap.put("1", "基础人才");
-        talentCategoryMap.put("2", "域外人才");
-        talentCategoryMap.put("3", "柔性引进人才");
+        talentCategoryMap.put("2", "柔性人才");
+        talentCategoryMap.put("3", "寓外人才");
         talentCategoryMap.put("4", "企业经营管理人才");
         talentCategoryMap.put("5", "高技能人才");
         talentCategoryMap.put("6", "文化社科人才");
         talentCategoryMap.put("7", "乡土人才");
         talentCategoryMap.put("8", "高学历人才");
-        talentCategoryMap.put("9", "专业技术人才");
-        talentCategoryMap.put("10", "尖端人才");
+        talentCategoryMap.put("9", "专技人才");
+        talentCategoryMap.put("10", "高端人才");
     }
 
     private static HashMap<String, String> talentHonourMap = new HashMap<>();
