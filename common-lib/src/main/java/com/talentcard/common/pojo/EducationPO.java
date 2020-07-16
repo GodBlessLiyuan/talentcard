e364d6a2804ed19113c55bff802b10cb27f856c9
xuchen <1009559594@qq.com>
2020-07-16 15:56:16 +0800

    增加字段

--- common-lib/src/main/java/com/talentcard/common/pojo/EducationPO.java
+++ common-lib/src/main/java/com/talentcard/common/pojo/EducationPO.java
@@ -1,24 +1,26 @@
 package com.talentcard.common.pojo;
 
 import java.io.Serializable;
+
 import lombok.Data;
 
 /**
  * t_education
- * @author 
+ *
+ * @author
  */
 @Data
 public class EducationPO implements Serializable {
     private Long educId;
 
-    private Integer education;
+    private Integer education = 0;
 
     private String school;
 
     /**
      * 1双一流；2海外人才；3啥也不是
      */
-    private Byte firstClass;
+    private Byte firstClass = 0;
 
     private String major;
 
@@ -30,20 +32,20 @@ public class EducationPO implements Serializable {
 
     /**
      * 1.正常使用
-2.注册没领卡（待领卡）
-3.发起过认证未审批（待审批）
-4.已有基础卡，且审批通过但未领卡（待领卡）
-5.基础卡正常使用
-9. 基本卡失效
-10.其他情况失效
+     * 2.注册没领卡（待领卡）
+     * 3.发起过认证未审批（待审批）
+     * 4.已有基础卡，且审批通过但未领卡（待领卡）
+     * 5.基础卡正常使用
+     * 9. 基本卡失效
+     * 10.其他情况失效
      */
     private Byte status;
 
     /**
      * 1 已认证；
-2 未认证；
-8 已认证，且是后来新加入的（编辑或者新增认证）
-10 本次不认证
+     * 2 未认证；
+     * 8 已认证，且是后来新加入的（编辑或者新增认证）
+     * 10 本次不认证
      */
     private Byte ifCertificate;
 
