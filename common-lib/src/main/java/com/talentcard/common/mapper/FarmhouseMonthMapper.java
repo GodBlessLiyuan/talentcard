4828bfd7c69156a92cf334382e1415b30938b268
liyuan <1627739285@qq.com>
2020-07-16 15:24:45 +0800

    问题反馈修改，农家乐的实时查询导出文件，日统计的初始化等3块

--- common-lib/src/main/java/com/talentcard/common/mapper/FarmhouseMonthMapper.java
+++ common-lib/src/main/java/com/talentcard/common/mapper/FarmhouseMonthMapper.java
@@ -1,11 +1,12 @@
 package com.talentcard.common.mapper;
 
 import com.talentcard.common.pojo.FarmhouseMonthPO;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 /**
  * FarmhouseMonthMapper继承基类
  */
-@Repository
+@Mapper
 public interface FarmhouseMonthMapper extends BaseMapper<FarmhouseMonthPO, Long> {
 }
