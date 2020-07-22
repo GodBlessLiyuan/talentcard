311a838530ac92c4f45104f1ee9afaa4063f6df4
liyuan <1627739285@qq.com>
2020-07-22 10:42:03 +0800

    旅游的实时查询，日统计（初始化和定时任务），头部统计信息，定时任务，测过版本

--- talentweb/src/main/java/com/talentcard/web/controller/TripScheduleTestController.java
+++ talentweb/src/main/java/com/talentcard/web/controller/TripScheduleTestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("TripScheduleTest")
-public class TripScheduleTest {
+public class TripScheduleTestController {
 //    @Autowired
 //    private TripDailyScheduleService tripDailyScheduleService;
 //    @PostMapping("testdaily_count")
