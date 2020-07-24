ef0e36c7db3cff9a8e53a456685095a7f04468b7
liyuan <1627739285@qq.com>
2020-07-24 15:48:20 +0800

    front测试

--- talentfront/src/test/java/com/talentcard/front/controller/BaseTest.java
+++ talentfront/src/test/java/com/talentcard/front/controller/BaseTest.java
@@ -3,12 +3,15 @@ package com.talentcard.front.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.talentcard.common.utils.StringToObjUtil;
 import com.talentcard.common.vo.ResultVO;
+import org.apache.poi.ss.formula.functions.T;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -33,7 +36,7 @@ public class BaseTest {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
-    public ResultVO<JSONObject> mockMvcPostUrlContent(String url, String jsonString) throws Exception {
+    public ResultVO<T> mockMvcPostUrlContent(String url, String jsonString) throws Exception {
         MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post(url)//get、post、put、delete
                 .contentType(MediaType.APPLICATION_JSON_UTF8)//设置编码格式
                 .content(jsonString)
@@ -42,17 +45,29 @@ public class BaseTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(MockMvcResultHandlers.print())
                 .andReturn();
-        ResultVO<JSONObject> resultVO = StringToObjUtil.strToObj(mvcResult.getResponse().getContentAsString(), ResultVO.class);
+        ResultVO<T> resultVO = StringToObjUtil.strToObj(mvcResult.getResponse().getContentAsString(), ResultVO.class);
         return resultVO;
     }
 
-    public ResultVO<JSONObject> moclMvcPostUrlParams(String url, Map<String,String> stringParamsMap) throws Exception {
+    public ResultVO<T> mockMvcPostUrlParams(String url, Map<String,String> stringParamsMap) throws Exception {
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post(url);
         for(Map.Entry<String,String> entry:stringParamsMap.entrySet()){
             requestBuilder.param(entry.getKey(),entry.getValue());
         }
         MvcResult mvcResult = mockMvc.perform(requestBuilder).andDo(MockMvcResultHandlers.print()).andExpect(status().isOk()).andReturn();
-        ResultVO<JSONObject> resultVO = StringToObjUtil.strToObj(mvcResult.getResponse().getContentAsString(),ResultVO.class);
+        ResultVO<T> resultVO = StringToObjUtil.strToObj(mvcResult.getResponse().getContentAsString(),ResultVO.class);
+        return resultVO;
+    }
+    /**
+     * 多文件上传还没有验证
+     * */
+    public ResultVO mockMvcFilePostUrlParams(String url,Map<String,String> stringParamsMap,MockMultipartFile... mockMultipartFile) throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post(url, mockMultipartFile);
+        for(Map.Entry<String,String> entry:stringParamsMap.entrySet()){
+            requestBuilder.param(entry.getKey(),entry.getValue());
+        }
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andDo(MockMvcResultHandlers.print()).andExpect(status().isOk()).andReturn();
+        ResultVO resultVO = StringToObjUtil.strToObj(mvcResult.getResponse().getContentAsString(), ResultVO.class);
         return resultVO;
     }
 }
--- talentfront/src/test/java/com/talentcard/front/controller/TalentControllerTest.java
+++ talentfront/src/test/java/com/talentcard/front/controller/TalentControllerTest.java
@@ -7,6 +7,7 @@ import com.talentcard.common.utils.StringToObjUtil;
 import com.talentcard.common.vo.ResultVO;
 import com.talentcard.front.FrontApplication;
 import com.talentcard.front.utils.AccessTokenUtil;
+import org.apache.poi.ss.formula.functions.T;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -108,49 +109,48 @@ public class TalentControllerTest extends BaseTest{
             //验证游客身份
             String openId = "fasdjflsdjf";
             map.put("openId",openId);//键值对
-            ResultVO<JSONObject> resultVO = super.moclMvcPostUrlParams(url, map);
+            ResultVO resultVO = super.mockMvcPostUrlParams(url, map);
             assertNotNull(resultVO);
             assertEquals(Integer.valueOf(1000),resultVO.getStatus());
             assertNotNull(resultVO.getData());
-            assertEquals("000000000",resultVO.getData().getString("code"));
+            assertEquals("000000000",((JSONObject)resultVO.getData()).getString("code"));
         }
-
         {
             //领取高级卡
             String openId = "oQetQ1ULYaj1Cg5UwNGbQ2hEGIQQ";
             map.put("openId",openId);
-            ResultVO<JSONObject> resultVO = super.moclMvcPostUrlParams(url, map);
+            ResultVO resultVO = super.mockMvcPostUrlParams(url, map);
             assertNotNull(resultVO);
             assertEquals(Integer.valueOf(1000),resultVO.getStatus());
             assertNotNull(resultVO.getData());
-            assertEquals("C010000001",resultVO.getData().getString("code"));
+            JSONObject jsonObject= (JSONObject) resultVO.getData();
+            assertEquals("C010000001",jsonObject.getString("code"));
             //注册过
-            assertEquals(Integer.valueOf(1),resultVO.getData().getInteger("status"));
+            assertEquals(Integer.valueOf(1),jsonObject.getInteger("status"));
             //不用换卡
-            assertEquals(Integer.valueOf(2),resultVO.getData().getInteger("ifChangeCard"));
+            assertEquals(Integer.valueOf(2),jsonObject.getInteger("ifChangeCard"));
             //未认证
-            assertEquals(Integer.valueOf(1),resultVO.getData().getInteger("ifCertificate"));
+            assertEquals(Integer.valueOf(1),jsonObject.getInteger("ifCertificate"));
         }
         {
             //验证注册未注册未领取卡的用户
             String openId = "oQetQ1SMJgI2-lfQ7Yvm6r2KjOqY";
             map.put("openId",openId);
-            ResultVO<JSONObject> resultVO = super.moclMvcPostUrlParams(url, map);
+            ResultVO resultVO = super.mockMvcPostUrlParams(url, map);
             assertNotNull(resultVO);
             assertEquals(Integer.valueOf(1000),resultVO.getStatus());
             assertNotNull(resultVO.getData());
-            assertEquals("010000002",resultVO.getData().getString("code"));
+            JSONObject jsonObject= (JSONObject) resultVO.getData();
+            assertEquals("010000002",jsonObject.getString("code"));
             //注册过
-            assertEquals(Integer.valueOf(1),resultVO.getData().getInteger("status"));
+            assertEquals(Integer.valueOf(1),jsonObject.getInteger("status"));
             //不用换卡
-            assertEquals(Integer.valueOf(2),resultVO.getData().getInteger("ifChangeCard"));
+            assertEquals(Integer.valueOf(2),jsonObject.getInteger("ifChangeCard"));
             //未认证
-            assertEquals(Integer.valueOf(2),resultVO.getData().getInteger("ifCertificate"));
+            assertEquals(Integer.valueOf(2),jsonObject.getInteger("ifCertificate"));
             //未认证中
-            assertEquals(Integer.valueOf(2),resultVO.getData().getInteger("ifInAudit"));
+            assertEquals(Integer.valueOf(2),jsonObject.getInteger("ifInAudit"));
         }
-
-
     }
 
     //验证码：230554，测试无误
@@ -163,49 +163,63 @@ public class TalentControllerTest extends BaseTest{
 //        assertEquals(new Integer(1000), resultVO.getStatus());
     }
 
-    //包含文件上传
+    /**
+     * 测试出错
+     * */
     @Test
     public void identification() throws Exception {
-
         {
-            JSONObject jsonObject=StringToObjUtil.strToObj("",JSONObject.class);
-            Set<String> strings = jsonObject.keySet();
-            for(String key:jsonObject.keySet()){
-                String value= (String) jsonObject.get(key);
-            }
             //	application/x-png或者image/png都可以；openId：gaojiyonghu
-            MockMultipartFile mockMultipartFile = new MockMultipartFile("educPicture", "图标.png","image/png",new FileInputStream("src\\test\\images\\share1.png"));
-            MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.fileUpload("/talent/identification").
-                    file(mockMultipartFile).
-                    param("openId", "oQetQ1Qe4bIp5h7mmsmNOwVXAn3U").
-                    param("education", String.valueOf(1)).//num类型，学历
-                    param("school", "北邮大学").//学校
-                    param("firstClass", "0").//是否为双一流
-                    param("major", "软件工程").
-                    param("graduateTime", "2020:1:30").//专业
-                    param("profTitleCategory", String.valueOf(1)).  //职称类别
-                    param("profQualityCategory", "1"). //职业资格 num
-                    param("honourId", "1") //人才荣誉
-            ).andDo(MockMvcResultHandlers.print()).andExpect(status().isOk()).andReturn();
-            ResultVO resultVO = StringToObjUtil.strToObj(mvcResult.getResponse().getContentAsString(), ResultVO.class);//list
+            String jsonObject="{\n" +
+                    "    \"openId\": \"gaojiyonghu\",\n" +
+                    "    \"education\": \"1\",\n" +
+                    "    \"school\": \"原味鸡\",\n" +
+                    "    \"firstClass\": 1,\n" +
+                    "    \"major\": 3,\n" +
+                    "    \"profQualityCategory\": \"1\",\n" +
+                    "    \"profQualityInfo\": \"1\",\n" +
+                    "    \"profTitleCategory\": \"1\",\n" +
+                    "    \"profTitleInfo\": \"召唤师峡谷\",\n" +
+                    "    \"honourId\": 2,\n" +
+                    "    \"graduateTime\": \"2020-02-35\",\n" +
+                    "    \"educPicture\": \"2\",\n" +
+                    "    \"educPicture2\": \"8\",\n" +
+                    "    \"educPicture3\": \"1\",\n" +
+                    "    \"profTitlePicture\": \"123\",\n" +
+                    "    \"profTitlePicture2\": \"5\",\n" +
+                    "    \"profTitlePicture3\": \"战争学院\",\n" +
+                    "    \"profQualityPicture\": \"1\",\n" +
+                    "    \"profQualityPicture2\": \"上单霸主\",\n" +
+                    "    \"profQualityPicture3\": \"0\",\n" +
+                    "    \"talentHonourPicture\": \"111\",\n" +
+                    "    \"talentHonourPicture2\": \"0\",\n" +
+                    "    \"talentHonourPicture3\": \"111\",\n" +
+                    "    \"fullTime\":1\n" +
+                    "}";
+            String url="/talent/identification";
+            ResultVO resultVO = super.mockMvcPostUrlContent(url, jsonObject);//list
             assertNotNull(resultVO);
-            assertEquals(new Integer(1000), resultVO.getStatus());
+            assertEquals(new Integer(1000), resultVO.getStatus());//2701；
+            /***
+             * 这数据没法测试了
+             *       ActivcateBO oldCard = talentMapper.activate(openId, (byte) 2, (byte) 1);
+             *             if (oldCard == null) {
+             *                 return new ResultVO(2701);
+             *             }
+             * */
         }
     }
-
+    /**
+     * 该数据库表和数据：Cause: org.h2.jdbc.JdbcSQLException: Column "e.educ_picture2" not found; SQL statement:
+     * */
     @Test
     public void findInfo() throws Exception {
-        String accessToken = AccessTokenUtil.getAccessToken();
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/talent/findInfo").
-                param("openId","gaojiyonghu")).
-                andDo(MockMvcResultHandlers.print()).andExpect(status().isOk()).andReturn();
-        ResultVO<JSONObject> resultVO = StringToObjUtil.strToObj(mvcResult.getResponse().getContentAsString(), ResultVO.class);
+        String url="/talent/findInfo";
+        Map<String,String> map=new HashMap<>();
+        map.put("openId","gaojiyonghu");
+        ResultVO<T> resultVO = super.mockMvcPostUrlParams(url, map);
         assertNotNull(resultVO);
         assertEquals(new Integer(1000),resultVO.getStatus());
-    }
-    //代码内容和updateUnionId一样，不用测试
-    @Test
-    public void fullUnion(){
     }
 
     //测试token，
@@ -214,11 +228,10 @@ public class TalentControllerTest extends BaseTest{
         {
             //token寻找在talentController的fullUnion
             String accessToken = AccessTokenUtil.getAccessToken();
-            MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/talent/updateUnionId").
-                    param("token", accessToken).
-                    param("openId", "oQetQ1ULYaj1Cg5UwNGbQ2hEGIQQ")).//随便找的一个
-                    andDo(MockMvcResultHandlers.print()).andExpect(status().isOk()).andReturn();
-            ResultVO<JSONObject> resultVO = StringToObjUtil.strToObj(mvcResult.getResponse().getContentAsString(), ResultVO.class);
+            Map<String,String> map=new HashMap<>();
+            map.put("openId", "oQetQ1ULYaj1Cg5UwNGbQ2hEGIQQ");
+            map.put("token", accessToken);
+            ResultVO<T> resultVO = super.mockMvcPostUrlParams("/talent/updateUnionId", map);
             assertNotNull(resultVO);
             assertEquals(new Integer(1000),resultVO.getStatus());
         }
