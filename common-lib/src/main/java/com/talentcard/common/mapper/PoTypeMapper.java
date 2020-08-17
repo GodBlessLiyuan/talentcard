f9ce6c790945afa73fcd9f01c2424128190cd7f8
hongxu.an <hongxu.an@vbooster.cn>
2020-08-17 19:52:45 +0800

    政策类型大类相关接口

--- common-lib/src/main/java/com/talentcard/common/mapper/PoTypeMapper.java
+++ common-lib/src/main/java/com/talentcard/common/mapper/PoTypeMapper.java
@@ -1,11 +1,44 @@
 package com.talentcard.common.mapper;
 
+import com.talentcard.common.bo.PolicyTypeBO;
 import com.talentcard.common.pojo.PoTypePO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * PoTypeMapper继承基类
  */
 @Mapper
 public interface PoTypeMapper extends BaseMapper<PoTypePO, Long> {
+    /**
+     * 首页根据政策类型和状态模糊查询
+     *
+     * @param reqData
+     * @return
+     */
+    List<PolicyTypeBO> pageQuery(Map<String, Object> reqData);
+    /**
+     * 首页根据政策类型id查出政策类型名称
+     *
+     * @param reqData
+     * @return
+     */
+    List<PolicyTypeBO> queryPtNameByPtId(@Param("reqData")List<String> reqData);
+    /**
+     * 查询所有的政策名称和对应的id，返给前端进行展示
+     *
+     * @param
+     * @return
+     */
+    List<PolicyTypeBO> queryExIdAndName();
+
+    /**
+     * 查询所有的政策名称和对应的id，返给前端进行展示
+     *
+     * @param
+     * @return
+     */
 }
