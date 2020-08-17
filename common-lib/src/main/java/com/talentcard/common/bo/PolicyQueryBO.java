89db3b85b5f5ae589b46776fb43c71262946ec2c
xuchen <1009559594@qq.com>
2020-08-17 08:53:44 +0800

    111

--- common-lib/src/main/java/com/talentcard/common/bo/PolicyQueryBO.java
+++ common-lib/src/main/java/com/talentcard/common/bo/PolicyQueryBO.java
@@ -3,6 +3,8 @@ package com.talentcard.common.bo;
 import com.talentcard.common.pojo.PolicyPO;
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * @author ChenXU
  * @version 1.0
@@ -10,9 +12,21 @@ import lombok.Data;
  * @description
  */
 @Data
-public class PolicyQueryBO extends PolicyPO {
-    //责任单位
-    private String roleName;
+public class PolicyQueryBO{
+    //政策名称
+    private String policyName;
+    //申请时间开启时间
+    private Date startTime;
+    //申请时间结束时间
+    private Date endTime;
+    //符合条件人数
+    private Integer meetConditionNumber;
     //政策类型名称
     private String policyTypeName;
+    //责任单位
+    private String roleName;
+    //更新时间
+    private Date updateTime;
+    //上下架
+    private Byte upLow;
 }
