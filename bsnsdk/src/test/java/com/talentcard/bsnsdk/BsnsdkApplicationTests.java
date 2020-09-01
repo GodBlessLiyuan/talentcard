32c1515d0ceca7bc2d9868548cb6aa3877401e13
haoliang.cui <haoliang.cui@rpahelper.com>
2020-09-01 17:52:49 +0800

    添加区块链模块

---
+++ bsnsdk/src/test/java/com/talentcard/bsnsdk/BsnsdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentcard.bsnsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsnsdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
