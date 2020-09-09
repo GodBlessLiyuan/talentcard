92a2f0d5c3a6814786eb9c1e97c5244f4f409fe7
xuchen <1009559594@qq.com>
2020-09-09 14:51:12 +0800

    时间参数bug

--- talentweb/src/main/java/com/talentcard/web/service/impl/EventServiceImpl.java
+++ talentweb/src/main/java/com/talentcard/web/service/impl/EventServiceImpl.java
@@ -191,12 +191,13 @@ public class EventServiceImpl implements IEventService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public ResultVO cancel(HttpSession httpSession, Long eventId, String opinion) {
         Long userId = (Long) httpSession.getAttribute("userId");
-        if (userId == null) {
-            // 用户过期
-            return ResultVO.notLogin();
-        }
+//        if (userId == null) {
+//            // 用户过期
+//            return ResultVO.notLogin();
+//        }
         EvEventPO evEventPO = evEventMapper.selectByPrimaryKey(eventId);
         if (evEventPO == null) {
             return new ResultVO(2750, "无此后台活动！");
@@ -455,7 +456,7 @@ public class EventServiceImpl implements IEventService {
             return;
         }
 
-        String newInterval = StringSortUtil.sort(evEventTimePO.getTimeInterval(),evEventPO.getTimeInterval(),"");
+        String newInterval = StringSortUtil.sort(evEventTimePO.getTimeInterval(),evEventPO.getTime(),"");
         //将新的时间段更新会时间占用表中
         evEventTimePO.setTimeInterval(newInterval);
         evEventTimeMapper.updateByPrimaryKeySelective(evEventTimePO);
